c134a0e4e2c975e42337b48cf5a8969181994bab
m.mentsel@gmail.com <Alabala1234!>
2025-03-17 22:28:59 +0200

    Change

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -2,11 +2,11 @@
 {
     public static class Constants
     {
-            public const string AppName = "MyTime";
+            public const string AppName = "mytime";
             public const string DefaultDateFormat = "yyyy-MM-dd";
             public const int MaxNameLength = 255;
             public const int MaxEmailLength = 255;
-            public const string CONNECTION_STRING = "Server=localhost;Database=MyTime;User=Miko;Password=Alabala1234!;";
+            public const string CONNECTION_STRING = "Server=localhost;Database=mytime;User=Miko;Password=Alabala1234!;";
 
         // Add more constants as needed
 
