399058dd9de16f6bd6b9297bf14ca5b5c5c80f28
m.mentsel@gmail.com <Alabala1234!>
2025-03-17 23:37:21 +0200

    Some changes

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -6,7 +6,7 @@
             public const string DefaultDateFormat = "yyyy-MM-dd";
             public const int MaxNameLength = 255;
             public const int MaxEmailLength = 255;
-            public const string CONNECTION_STRING = "Server=localhost;Database=mytime;User=Miko;Password=Alabala1234!;";
+            public const string CONNECTION_STRING = "Server=localhost;Database=MyTime;User=Miko;Password=Alabala1234!;";
 
         // Add more constants as needed
 
